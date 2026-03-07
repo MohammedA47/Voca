fc8b0eb5afa365246bbcf704d25094064bfd960b
MohammedA47 <pstomohammedasiri@gmail.com>
2026-03-07 03:55:32 +0300

    feat: Initialize Android platform for Capacitor project, including web assets and build configuration.

---
+++ android/app/src/main/assets/public/assets/label-_dJrkIVu.js
@@ -0,0 +1,11 @@
+import{c as s,r as o,j as l,P as n,q as c,s as f}from"./index-uGRdrVmR.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=s("ArrowLeft",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=s("LoaderCircle",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]);var m="Label",d=o.forwardRef((e,t)=>l.jsx(n.label,{...e,ref:t,onMouseDown:a=>{var r;a.target.closest("button, input, select, textarea")||((r=e.onMouseDown)==null||r.call(e,a),!a.defaultPrevented&&a.detail>1&&a.preventDefault())}}));d.displayName=m;var i=d;const u=f("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),x=o.forwardRef(({className:e,...t},a)=>l.jsx(i,{ref:a,className:c(u(),e),...t}));x.displayName=i.displayName;export{w as A,y as L,x as a};
